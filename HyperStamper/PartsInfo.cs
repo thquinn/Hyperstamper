d79503a749660825466bf29fdc3ad65e03d5b7fe
Tom Quinn <thomashquinn@gmail.com>
2015-04-26 16:04:00 -0400

    Added products and stubs

--- HyperStamper/PartsInfo.cs
+++ HyperStamper/PartsInfo.cs
@@ -8,16 +8,26 @@ namespace HyperStamper
 {
     public class PartsInfo
     {
-        // The product
-        public Part product;
+        public static readonly string STRUCTURAL_FRAME = "XXXXX\r\nXOOOX\r\nXOOOX\r\nXOOOX\r\nXXXXX\r\n\r\nXOOOX\r\nOOOOO\r\nOOOOO\r\nOOOOO\r\nXOOOX\r\n\r\nXOOOX\r\nOOOOO\r\nOOOOO\r\nOOOOO\r\nXOOOX\r\n\r\nXOOOX\r\nOOOOO\r\nOOOOO\r\nOOOOO\r\nXOOOX\r\n\r\nXXXXX\r\nXOOOX\r\nXOOOX\r\nXOOOX\r\nXXXXX";
+        public static readonly string DOCKING_CLAMP = "XXXXX\r\nXXXXX\r\nXXXXX\r\nXXXXX\r\nXXXXX\r\n\r\nOOOOO\r\nOOOOO\r\nOOXOO\r\nOOOOO\r\nOOOOO\r\n\r\nOOOOO\r\nOOOOO\r\nXXXXX\r\nOOOOO\r\nOOOOO\r\n\r\nOOOOO\r\nOOOOO\r\nXOOOX\r\nOOOOO\r\nOOOOO\r\n\r\nOOOOO\r\nOOOOO\r\nXOOOX\r\nOOOOO\r\nOOOOO";
+
         // Maps from any part to a list of its rotations in canonical order -- rotations whose bit arrays evaluate to
         // lower numbers are more canonical than those whose bit arrays evaluate to larger numbers.
         public Dictionary<Part, List<Part>> partRotations;
+        // Returns true if the part (canonical rotation) is a substructure of the product at any rotation, false otherwise.
+        public Dictionary<Part, bool> isSubproductMemo;
+        // The product we're trying to create by combining parts.
+        public Part product;
 
         public PartsInfo(String productString)
         {
+            // Initialize dictionaries.
+            partRotations = new Dictionary<Part, List<Part>>();
+            isSubproductMemo = new Dictionary<Part, bool>();
+
             // TODO: Check that the product completely fills the area given.
 
+            // Read the product string.
             List<bool> bools = new List<bool>();
             byte length = 0, width = 0, height = 0;
             string[] zSlices = productString.Split(new string[] { "\r\n\r\n" }, StringSplitOptions.RemoveEmptyEntries);
@@ -40,21 +50,45 @@ namespace HyperStamper
             for (int i = 0; i < bitArray.Length; i++)
                 bitArray.Set(i, bools[i]);
             product = new Part(length, width, height, bitArray);
-
-            partRotations = new Dictionary<Part, List<Part>>();
+            // Set the product to the canonical rotation of itself.
+            Analyze(product);
+            product = partRotations[product][0];
         }
 
+        // Analyzes all relevant information about the given part and stores it to prevent repeat analysis.
         public void Analyze(Part part)
         {
-            // TODO: Check that the part is shifted as close to the axes as possible.
+            // TODO: Check that the part is shifted as close to the axes as possible, throw an error otherwise.
+
+            // If we've already analyzed this part, we don't need to do it again.
+            if (partRotations.ContainsKey(part))
+                return;
 
             // Find all rotations of part and insert into partRotations with each rotation as key.
             List<Part> rotations = part.GetRotations();
             foreach (Part rotation in rotations)
                 partRotations.Add(rotation, rotations);
-            
-            // TODO: Check if each rotation is a part of the product (or the entire product). If any are, they all are -- but you might want to canonicalize differently in that case.
-            // In fact, we might only want to store rotations that are subparts(?)
+
+
+        }
+
+        // Returns the canonical rotations of all parts that can be produced by welding the two inputs together that
+        // are substructures of the product.
+        public List<Part> AllSubproductCombinations(Part first, Part second)
+        {
+            // TODO: Implement.
+            return null;
+        }
+
+        // Check if each rotation is a part of the product (or is the entire product). If any are, they all are. Store
+        // the result in isSubproductMemo under the canonical rotation.
+        public bool IsSubproduct(Part part)
+        {
+            if (isSubproductMemo.ContainsKey(part))
+                return isSubproductMemo[part];
+
+            // TODO: Implement.
+            return false;
         }
     }
 }
--- HyperStamper/Program.cs
+++ HyperStamper/Program.cs
@@ -9,6 +9,12 @@ namespace HyperStamper
     {
         static void Main(string[] args)
         {
+            // Initialize our PartsInfo with the desired product.
+            PartsInfo info = new PartsInfo(PartsInfo.DOCKING_CLAMP);
+
+            // TODO: Create a hashable collection of parts.
+
+            // TODO: Do an iterative search on the collection of parts to see if it can be combined into the product.
         }
     }
 }
